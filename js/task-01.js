3af5683414bac12ba9d296655d2f5fec6bc24d27
OlegKrivenko <krivenko.olegus@gmail.com>
2023-02-09 11:08:57 +0200

    правки в 1; 9 та 10 (функція кольору)

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ const arrayOfCategories = listOfCategories.children;
 console.log(`Number of categories: ${arrayOfCategories.length}`);
 
 [...arrayOfCategories].forEach(category => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements:${category.lastElementChild.children.length}`);
+  console.log(`Category: ${category.querySelector('h2').textContent}`);
+  console.log(`Elements:${category.querySelectorAll('li').length}`);
   return;
 });
